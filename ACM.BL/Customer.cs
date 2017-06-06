75d536456406a213394926a60f68b7cf79505c58
kimikomu <kimikomu@gmail.com>
2017-06-06 11:42:01 -0700

    Added business layer and added Customer class to it.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL	// Acme Content Manager Buisiness Layer
+{
+    public class Customer
+    {
+	    private string _lastName;
+
+	    public string LastName
+	    {
+		    get { return _lastName; }
+		    set { _lastName = value; }
+	    }
+
+		public string FirstName { get; set; }
+
+	    public string EmailAddress { get; set; }
+
+	    public int CustomerId { get; private set; }
+
+	    public string FullName		// does not need a backing field bc it does not require storage of any data
+	    {
+		    get { return LastName + "," + FirstName; }	
+	    }
+	}
+}
