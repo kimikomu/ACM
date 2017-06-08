2cb65cb2e5e6288b6fdfadbc97b6d1866934901f
kimikomu <kimikomu@gmail.com>
2017-06-08 13:05:04 -0700

    Created ProductRepository class. Moved Retrieve and Save methods from Product class to ProductRepsoitory class. Added temporary code to Retrieve method for testing purposes.

--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-	class Product
+	public class Product
 	{
 		public Product()
 		{
@@ -18,29 +18,10 @@ namespace ACM.BL
 			this.ProductId = productId;
 		}
 
-		public  Decimal? CurrentPrice { get; set; }	// nullable type is a value type (int, decimal) allows definition of a value OR a null. Use when the code needs to distinguish between "not set" and 0;
+		public Decimal? CurrentPrice { get; set; }	// nullable type is a value type (int, decimal) allows definition of a value OR a null. Use when the code needs to distinguish between "not set" and 0;
 		public int ProductId { get; private set; }
 		public string ProductDescription { get; set; }
 		public string ProductName { get; set; }
-
-		/// <summary>
-		/// Retrieve one customer.
-		/// </summary>
-		public Product Retrieve(int productId)
-		{
-			// code that retrieves the defined product
-			return new Product();
-		}
-
-		/// <summary>
-		/// Saves the current customer.
-		/// </summary>
-		/// <returns></returns>
-		public bool Save()
-		{
-			// code that saves the defined product
-			return true;
-		}
 
 		// method to validate property values
 		public bool Validate()
---
+++ ACM.BL/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+	public class ProductRepository
+	{
+		/// <summary>
+		/// Retrieve one product.
+		/// </summary>
+		public Product Retrieve(int productId)
+		{
+			// create the instance of the Product class and pass in the requested id
+			Product product = new Product(productId);
+
+			// code that retrieves the defined product
+
+			// temporary hard coded values to return a populated product
+			if (productId == 2)
+			{
+				product.ProductName = "Sunflowers";
+				product.ProductDescription = "Assorted Sizes";
+				product.CurrentPrice = 15.96M;
+			}
+
+			return product;
+		}
+
+		/// <summary>
+		/// Saves the current product.
+		/// </summary>
+		/// <returns></returns>
+		public bool Save()
+		{
+			// code that saves the defined product
+			return true;
+		}
+	}
+}
