3505119ee9386895efc4567010e2b203bcee79b5
kimikomu <kimikomu@gmail.com>
2017-07-27 17:46:16 -0700

    Create display classes for orders and order items.

--- ACM.BL/OrderRepository.cs
+++ ACM.BL/OrderRepository.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ACM.BL
 {
@@ -23,6 +20,60 @@ namespace ACM.BL
 
 			return new Order();
 		}
+
+
+		public OrderDisplay RetrieveOrderDisplay(int orderId)
+		{
+			OrderDisplay orderDisplay = new OrderDisplay();
+
+			// Code that retrieves the defined order fields
+
+			// Temporary hard coded data
+			if (orderId == 10)
+			{
+				orderDisplay.FirstName = "Bilbo";
+				orderDisplay.LastName = "Baggins";
+				orderDisplay.OrderDate = new DateTimeOffset(2014, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+				orderDisplay.ShippingAddress = new Address()
+				{
+					AddressType = 1,
+					StreetLine1 = "Bag End",
+					StreetLine2 = "Bagshot Row",
+					City = "Hobbiton",
+					State = "Shire",
+					Country = "Middle Earth",
+					PostalCode = "144"
+				};
+			}
+
+			orderDisplay.OrderDisplayItemList = new List<OrderDisplayItem>();
+
+			// Code that retrieves the order items
+
+			// Temporary hard coded data
+			if (orderId == 10)
+			{
+				var orderDisplayItem = new OrderDisplayItem()
+				{
+					ProductName = "Sunflowers",
+					PurchasePrice = 15.96M,
+					OrderQuantity = 2
+				};
+				orderDisplay.OrderDisplayItemList.Add(orderDisplayItem);
+
+				orderDisplayItem = new OrderDisplayItem()
+				{
+					ProductName = "Rake",
+					PurchasePrice = 6M,
+					OrderQuantity = 1
+				};
+				orderDisplay.OrderDisplayItemList.Add(orderDisplayItem);
+			}
+
+			return orderDisplay;
+
+		}
+
 
 		/// <summary>
 		/// Saves the current customer.
